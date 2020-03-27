b6050886b8c2ebe2e84e56a17351670287dea065
josiah <josiahdamiwilliams@gmail.com>
2020-03-27 17:42:15 +0100

    finish sprint

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
+// require('dotenv').config();
 const jwt = require("jsonwebtoken");
 
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
-
 module.exports = (req, res, next) => {
-  const secret = process.env.JWT_SECRET || "A secret lives here";
-  const token = req.headers.authorization;
-  if (token) {
-      jwt.verify(token, secret, (error, decodedToken) => {
-          if (error) {
-              res.status(401).json({ message: "Something went wrong:- " + error.message})
-          } else {
-              req.user = decodedToken.user
-              next()
-          }
-      })
+	res.status(401).json({ you: "shall not pass!" });
+	const token = req.headers.authorization;
+
+	if (token) {
+		jwt.verify(
+			token,
+			process.env.JWT_SECRETE || "thesecret",
+			(err, decoded) => {
+				if (err) {
+					res.status(500).json({ message: "bad token" });
+				} else {
+					req.decodedToken = decoded;
+					next();
+				}
+			}
+		);
   } else {
     res.status(401).json({ you: 'shall not pass!' });
   }
